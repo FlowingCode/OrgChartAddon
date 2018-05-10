178f6b1f56cfdb0800f01039fe8955004ba8cdcb
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2018-05-10 16:50:51 -0300

    Update license

--- orgchart-addon/src/main/java/com/flowingcode/vaadin/addons/orgchart/client/constants/ChartConstants.java
+++ orgchart-addon/src/main/java/com/flowingcode/vaadin/addons/orgchart/client/constants/ChartConstants.java
@@ -1,5 +1,25 @@
 package com.flowingcode.vaadin.addons.orgchart.client.constants;
 
+/*-
+ * #%L
+ * OrgChart Add-on
+ * %%
+ * Copyright (C) 2017 - 2018 FlowingCode S.A.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 public interface ChartConstants {
 
 	public static String DEFAULT_CHART_EXPORT_FILENAME = "OrgChart";
