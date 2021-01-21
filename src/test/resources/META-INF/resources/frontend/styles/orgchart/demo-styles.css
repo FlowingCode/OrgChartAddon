ce72c32bafac27e0f58b3e6d6c0ffbe7f731e872
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-01-21 10:41:28 -0300

    chore: update license headers

--- src/test/resources/META-INF/resources/frontend/styles/orgchart/demo-styles.css
+++ src/test/resources/META-INF/resources/frontend/styles/orgchart/demo-styles.css
@@ -2,7 +2,7 @@
  * #%L
  * OrgChart Add-on
  * %%
- * Copyright (C) 2017 - 2019 Flowing Code S.A.
+ * Copyright (C) 2017 - 2020 Flowing Code S.A.
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
